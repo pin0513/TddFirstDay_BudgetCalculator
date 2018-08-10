b96eea923439db120beba22ea9e2a884f88dcc2a
paul_iphuang <paul_iphunag@tutorabc.com>
2018-08-10 17:04:57 +0800

    done

---
+++ BudgetCalculator/BudgetCalculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TennisGame01
+{
+    public class BudgetCalculator
+    {
+        private IBudgetRepo _repo;
+
+        public BudgetCalculator(IBudgetRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public decimal TotalAmount(DateTime start, DateTime end)
+        {
+            if (start > end)
+                throw new ArgumentException();
+
+            var budgetList = _repo.GetAll();
+            if (budgetList.Count == 0)
+            {
+                return 0;
+            }
+
+            if (start.ToString("yyyyMM") == end.ToString("yyyyMM"))
+            {
+                return GetBudgetPerMonth(budgetList, start, end);
+            }
+            else
+            {
+                var firstMonthEndDay = new DateTime(start.Year, start.Month, DateTime.DaysInMonth(start.Year, start.Month));
+                var lastMonthStartDay = new DateTime(end.Year, end.Month, 1);
+                var amount = GetBudgetPerMonth(budgetList,start, firstMonthEndDay) + GetBudgetPerMonth(budgetList, lastMonthStartDay, end);
+                var targetStart = start.AddMonths(1);
+                var targetEnd = end.AddMonths(-1);
+                while (targetStart < targetEnd)
+                {
+                    amount += budgetList.ContainsKey(targetStart.ToString("yyyyMM")) ? budgetList[targetStart.ToString("yyyyMM")] : 0;
+                    targetStart = targetStart.AddMonths(1);
+
+                }
+
+                return amount;
+            }
+
+            if (!budgetList.ContainsKey(start.ToString("yyyyMM")))
+            {
+                return 0;
+            }
+
+            throw new ArgumentException("bad...");
+        }
+
+        private decimal GetBudgetPerMonth(Dictionary<string, decimal> budgetList, DateTime start, DateTime end)
+        {
+            if (budgetList.ContainsKey(start.ToString("yyyyMM")))
+            {
+                int dayDiffs = (end - start).Days;
+                var amount = budgetList[start.ToString("yyyyMM")];
+
+                var daysOfMonth = DateTime.DaysInMonth(start.Year, start.Month);
+                return (amount / daysOfMonth) * (dayDiffs + 1);
+            }
+            else
+                return 0;
+        }
+    }
+}
---
+++ BudgetCalculator/BudgetTests.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace TennisGame01
+{
+    /// <summary>
+    /// BudgetTests 的摘要說明
+    /// </summary>
+    [TestClass]
+    public class BudgetTests
+    {
+        public BudgetTests()
+        {
+            //
+            // TODO: 在此加入建構函式的程式碼
+            //
+        }
+
+        [TestMethod]
+        public void WithNoBudgetData()
+        {
+            var budgetCalculator = new BudgetCalculator(new EmptyBudgetRepository());
+            var budget = budgetCalculator.TotalAmount(DateTime.MinValue, DateTime.MinValue);
+            Assert.AreEqual(0, budget);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void WrongStartEnd()
+        {
+            var budgetCalculator = new BudgetCalculator(new TestDataBudgetRepository());
+            var budget = budgetCalculator.TotalAmount(new DateTime(2018, 12, 31), new DateTime(2017, 12, 31));
+        }
+
+        [TestMethod]
+        public void AllOutOfRangeBudget()
+        {
+            var budgetCalculator = new BudgetCalculator(new TestDataBudgetRepository());
+            var budget = budgetCalculator.TotalAmount(new DateTime(2017, 12, 31), new DateTime(2017, 12, 31));
+            Assert.AreEqual(0, budget);
+        }
+
+        [TestMethod]
+        public void SingleMonthHaveBudget()
+        {
+            var budgetCalculator = new BudgetCalculator(new TestDataBudgetRepository());
+            var budget = budgetCalculator.TotalAmount(new DateTime(2018, 01, 01), new DateTime(2018, 01, 31));
+            Assert.AreEqual(310, budget);
+        }
+
+        [TestMethod]
+        public void SingleMonthDaysHaveBudget()
+        {
+            var budgetCalculator = new BudgetCalculator(new TestDataBudgetRepository());
+            var budget = budgetCalculator.TotalAmount(new DateTime(2018, 01, 01), new DateTime(2018, 01, 02));
+            Assert.AreEqual(20, budget);
+        }
+
+        [TestMethod]
+        public void CrossMonthHaveBudget()
+        {
+            var budgetCalculator = new BudgetCalculator(new TestDataBudgetRepository());
+            var budget = budgetCalculator.TotalAmount(new DateTime(2018, 03, 31), new DateTime(2018, 04, 01));
+            Assert.AreEqual(50, budget);
+        }
+
+        [TestMethod]
+        public void CrossYearHaveBudget()
+        {
+            var budgetCalculator = new BudgetCalculator(new TestDataBudgetRepository());
+            var budget = budgetCalculator.TotalAmount(new DateTime(2017, 08, 31), new DateTime(2018, 06, 01));
+            Assert.AreEqual(1830, budget);
+        }
+        [TestMethod]
+        public void CrossmMonthNotBudget()
+        {
+            var budgetCalculator = new BudgetCalculator(new TestDataBudgetRepository());
+            var budget = budgetCalculator.TotalAmount(new DateTime(2018, 2, 15), new DateTime(2018, 03, 15));
+            Assert.AreEqual(300, budget);
+        }
+        [TestMethod]
+        public void OutOfRangBudget()
+        {
+            var budgetCalculator = new BudgetCalculator(new TestDataBudgetRepository());
+            var budget = budgetCalculator.TotalAmount(new DateTime(2019, 1, 1), new DateTime(2019, 12, 31));
+            Assert.AreEqual(0, budget);
+        }
+    }
+
+    public interface IBudgetRepo
+    {
+        Dictionary<string, decimal> GetAll();
+    }
+
+    public class TestDataBudgetRepository : IBudgetRepo
+    {
+        public TestDataBudgetRepository()
+        {
+        }
+
+        public Dictionary<string, decimal> GetAll()
+        {
+            return new Dictionary<string, decimal>()
+            {
+                { "201801", 310},
+                { "201803", 620},
+                { "201804", 900},
+            };
+        }
+    }
+
+    public class EmptyBudgetRepository : IBudgetRepo
+    {
+        public EmptyBudgetRepository()
+        {
+        }
+
+        public Dictionary<string, decimal> GetAll()
+        {
+            return new Dictionary<string, decimal>();
+        }
+    }
+}
